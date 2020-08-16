74a636bf52f461a4d997281b132b62098654b592
unknown <saifhusain990@gmail.com>
2020-08-16 19:41:22 +0530

    migration db

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    productId = models.AutoField
+    productName = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    publishDate = models.DateField()
