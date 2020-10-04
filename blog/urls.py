c5032a5a35862f051cf424f6aed548c1e88e0a5a
saif husain <saifhusain990@gmail.com>
2020-10-04 22:38:45 +0530

    blogPost

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [
-    path('', views.index, name='blog Home')
+    path('', views.index, name='blogHome'),
+    path('blogpost/', views.blogPost, name='blogPost')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'blog/index.html')
+
+def blogPost(request):
+    return render(request, 'blog/blogPost.html')
