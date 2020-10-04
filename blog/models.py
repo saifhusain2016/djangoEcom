e9f7fe64ff6192ede68dcf17e4ac3e141b76a919
saif husain <saifhusain990@gmail.com>
2020-10-05 00:07:02 +0530

    blogpost db home

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Blogpost(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    head_0 = models.CharField(max_length=200, default="")
+    chead_0 = models.CharField(max_length=1000, default="")
+    head_1 = models.CharField(max_length=500, default="")
+    chead_1 = models.CharField(max_length=1000, default="")
+    head_2 = models.CharField(max_length=500, default="")
+    chead_2 = models.CharField(max_length=5000, default="")
+    pub_date = models.DateTimeField()
+    thumbnail = models.ImageField(upload_to="blog/images/thumbnails", default="")
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from .models import Blogpost
 
 def index(request):
-    return render(request, 'blog/index.html')
+    data = Blogpost.objects.all()
+    return render(request, 'blog/index.html', {"posts":data})
 
 def blogPost(request):
     return render(request, 'blog/blogPost.html')
